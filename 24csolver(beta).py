b58e55a8cd0ab6e5a63f465c22c20b8d514ed022
hanskarta <63604037+hanskarta@users.noreply.github.com>
2024-03-21 17:34:14 +0700

    Update 24csolver(beta).py

--- 24csolver(beta).py
+++ 24csolver(beta).py
@@ -1,6 +1,6 @@
 from itertools import permutations, product
 
-//this version doesnt have any feature yet justa regular 24 card solver
+#this version doesnt have any feature yet justa regular 24 card solver
 
 def card_value(card):
     if card in ['J', 'Q', 'K']:
